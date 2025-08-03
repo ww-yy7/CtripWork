9454c2bbcf2c306f6ea47b81ae06c61b5ad2c64b
ww-yy7 <2205768934@qq.com>
2025-08-03 21:34:51 +0800

    在App组件中添加useEffect钩子以处理副作用逻辑

--- backendReview/src/App.jsx
+++ backendReview/src/App.jsx
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    // 你的副作用逻辑，比如获取数据、设置定时器等
+    console.log('Component mounted');
+
+    return () => {
+      // 可选的清理逻辑
+      console.log('Component unmounted');
+    };
+  }, []);
+
   return (
     <>
       <div>hello world</div>
-      useEffect(() => {
-        
-      }, []);
     </>
-  )
+  );
 }
 
-export default App
+export default App;
